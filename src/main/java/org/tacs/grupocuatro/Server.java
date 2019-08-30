75f7752242def98cb7515db8574047f3bc7958aa
Cristian Marcelo Soria <sbcristiansoria@gmail.com>
2019-08-30 15:11:12 -0300

    Rutas de autenticación.

--- src/main/java/org/tacs/grupocuatro/Server.java
+++ src/main/java/org/tacs/grupocuatro/Server.java
@@ -1,47 +1,31 @@
 package org.tacs.grupocuatro;
 
 import io.javalin.Javalin;
+import io.javalin.core.security.Role;
 import io.javalin.http.Context;
+import io.javalin.http.Handler;
 import org.tacs.grupocuatro.controller.AuthenticationController;
 import org.tacs.grupocuatro.controller.RepositoryController;
 import org.tacs.grupocuatro.controller.UserController;
 import org.tacs.grupocuatro.entity.ApplicationRoles;
-import org.tacs.grupocuatro.entity.Repository;
+
+import java.util.Set;
 
 import static io.javalin.apibuilder.ApiBuilder.*;
 import static io.javalin.core.security.SecurityUtil.roles;
 import static org.tacs.grupocuatro.entity.ApplicationRoles.*;
 
 public class Server {
-
-    private static ApplicationRoles getUserRole(Context ctx) {
-        // debería llamar al userDAO con el id del usuario
-        // y devolver el rol de ese usuario
-        return ADMIN;
-    }
-
-    private static void authenticateGitHub() {
-        // hacer alguna magia para autenticarse en github si no estamos autenticados
-        // probablemente sea un singleton, ya que si eventualmente tenemos muchas
-        // instancias, vamos a tener que autenticar en todas las instancias anyways
-    }
+    // poner en otro lado?
+    public static boolean DEBUG = true;
 
     public static void main(String[] args) {
         Javalin app = Javalin.create().start(8080);
-        app.config.accessManager((handler, ctx, permittedRoles) -> {
-            var role = getUserRole(ctx);
+        app.config.accessManager(Server::handleAuth);
 
-            if (permittedRoles.contains(role)) {
-                ctx.attribute("role", role);
-                handler.handle(ctx);
-            } else {
-                ctx.status(401).result("Unauthorized.");
-            }
-        });
-
-        app.get("/", ctx -> ctx.html("Hello, Javelin."));
         app.before(ctx -> authenticateGitHub());
 
+        app.get("/", ctx -> ctx.html("Hello, Javelin."));
         app.routes(() -> {
             post("/signup", AuthenticationController::signup);
             post("/login", AuthenticationController::login);
@@ -66,4 +50,26 @@ public class Server {
         });
     }
 
+    private static ApplicationRoles getUserRole(Context ctx) {
+        // debería llamar al userDAO con el id del usuario
+        // y devolver el rol de ese usuario
+        return ADMIN;
+    }
+
+    private static void authenticateGitHub() {
+        // hacer alguna magia para autenticarse en github si no estamos autenticados
+        // probablemente sea un singleton, ya que si eventualmente tenemos muchas
+        // instancias, vamos a tener que autenticar en todas las instancias anyways
+    }
+
+    private static void handleAuth(Handler handler, Context ctx, Set<Role> permittedRoles) throws Exception {
+        var role = getUserRole(ctx);
+
+        if (permittedRoles.contains(role) || DEBUG && role == ADMIN) {
+            ctx.attribute("role", role);
+            handler.handle(ctx);
+        } else {
+            ctx.status(401).result("Unauthorized.");
+        }
+    }
 }
--- src/main/java/org/tacs/grupocuatro/controller/AuthenticationController.java
+++ src/main/java/org/tacs/grupocuatro/controller/AuthenticationController.java
@@ -1,14 +1,51 @@
 package org.tacs.grupocuatro.controller;
 
 import io.javalin.http.Context;
+import org.tacs.grupocuatro.JsonResponse;
 
 public class AuthenticationController {
     public static void signup(Context ctx) {
+        // parsamos el cuerpo como una clase, javalin valida
+        var user = ctx.bodyAsClass(AuthenticationPayload.class);
+        ctx.json(new JsonResponse("Signed up!").with(user));
     }
 
     public static void login(Context ctx) {
+        // cookiestore funciona entre diferentes instancias
+        // eventualmente debería ser algun hash o un jwt
+        var userId = 1234;
+        ctx.cookieStore("userId", userId);
+        ctx.json(new JsonResponse("Logged in!").with(userId));
     }
 
     public static void logout(Context ctx) {
+        ctx.clearCookieStore();
+        ctx.json(new JsonResponse("Logged out!"));
     }
 }
+
+class AuthenticationPayload {
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String toString() {
+        return "Username: " + username + ", Password: " + password;
+    }
+}
+
