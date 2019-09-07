a2d6fa133392f2c1f7ba12baa2b7b78890a627f2
juancrespo18 <juanmacres87@gmail.com>
2019-09-07 12:22:47 -0300

    Agrego interfaz Dao, UserDao y modelo de User.

---
+++ src/main/java/org/tacs/grupocuatro/DAO/Dao.java
@@ -0,0 +1,17 @@
+package org.tacs.grupocuatro.DAO;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface Dao<T> {
+
+    Optional<T> get(long id);
+
+    List<T> getAll();
+
+    void save(T t);
+
+    void update(T t);
+
+    void delete(T t);
+}
--- src/main/java/org/tacs/grupocuatro/entity/User.java
+++ src/main/java/org/tacs/grupocuatro/entity/User.java
@@ -1,4 +1,66 @@
 package org.tacs.grupocuatro.entity;
 
+import java.util.Date;
+import java.util.List;
+import java.util.Set;
+
 public class User {
+    private int id;
+
+    private String username;
+
+    private Date lastLogin;
+
+    private long password;
+
+    private Set<String> favRepos;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Date getLastLogin() {
+        return lastLogin;
+    }
+
+    public void setLastLogin(Date lastLogin) {
+        this.lastLogin = lastLogin;
+    }
+
+    public long getPassword() {
+        return password;
+    }
+
+    public void setPassword(long password) {
+        this.password = password;
+    }
+
+    public Set<String> getFavRepos() {
+        return favRepos;
+    }
+
+    public void setFavRepos(Set<String> favRepos) {
+        this.favRepos = favRepos;
+    }
+
+    public void addFavRepo(String repo){
+        try {
+            this.favRepos.add(repo);
+        }
+        catch (Exception e) {
+            return;
+        }
+    }
 }
