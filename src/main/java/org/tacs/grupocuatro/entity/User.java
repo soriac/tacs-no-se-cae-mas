3820795e0d9b752597e5a94ba2dd7fd241313fdf
lucascharlab <lucascharlab@gmail.com>
2019-09-14 20:12:30 -0300

    loggable y admin

--- src/main/java/org/tacs/grupocuatro/entity/User.java
+++ src/main/java/org/tacs/grupocuatro/entity/User.java
@@ -1,35 +1,13 @@
 package org.tacs.grupocuatro.entity;
 
 import java.util.Date;
-import java.util.List;
 import java.util.Set;
 
-public class User {
-    private int id;
-
-    private String username;
+public class User extends Loggable{
 
     private Date lastLogin;
 
-    private long password;
-
     private Set<String> favRepos;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
 
     public Date getLastLogin() {
         return lastLogin;
@@ -37,14 +15,6 @@ public class User {
 
     public void setLastLogin(Date lastLogin) {
         this.lastLogin = lastLogin;
-    }
-
-    public long getPassword() {
-        return password;
-    }
-
-    public void setPassword(long password) {
-        this.password = password;
     }
 
     public Set<String> getFavRepos() {
